9152d0145eed17dac1e73fbf6c207b42e0a7743e
Shahbaz <shahbaz6295@gmail.com>
2024-12-24 03:11:57 +0530

    version-27

--- auth.ts
+++ auth.ts
@@ -1,98 +1,117 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import { compare } from "bcryptjs";
-import connectDB from "@/config/db";
-import { User } from "@/models/User";
-import { IUser } from "@/components/expandableCards/card";
+'use server'
+import NextAuth, { CredentialsSignin } from "next-auth"
+import Credentials from "next-auth/providers/credentials" 
+import connectDB from "./config/db";
+import { User } from "./models/User";
+import {compare} from "bcryptjs";
 
-// Define a type for the credentials
-interface Credentials {
-  email: string;
-  password: string;
-}
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
-const options: NextAuthOptions = {
+export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
+    Google({
+      clientId:process.env.GOOGLE_CLIENT_ID,
+      clientSecret:process.env.GOOGLE_CLIENT_SECRET
+    })
+    ,
+    GitHub({
+      clientId:process.env.GITHUB_CLIENT_ID,
+      clientSecret:process.env.GITHUB_CLIENT_SECRET
+    })
+    ,
+    Credentials({
+      name:'Credentials',
+
+      credentials:{
+        email:{label:"Email",type:"email"},
+        password:{label:"Password",type:"password"}
       },
-      authorize: async (credentials: Credentials | undefined) => {
-        if (!credentials) {
-          throw new Error("Please provide email and password");
+
+      authorize: async(credentials)=>{
+        const email=credentials.email as string | undefined;
+        const password=credentials.password as string | undefined
+
+        if(!email || !password){
+          throw new CredentialsSignin("Please provide email and password")
         }
 
-        const { email, password } = credentials;
         await connectDB();
 
-        const user = await User.findOne({ email }).select("+password +role");
-        if (!user) {
-          throw new Error("Invalid email or password");
+        const user=await User.findOne({email}).select("+password +role")
+
+        if(!user){
+          throw new CredentialsSignin({cause:"invalid email or password"})
         }
 
-        const isMatched = await compare(password, user.password);
-        if (!isMatched) {
-          throw new Error("Invalid email or password");
-        }
+        const isMatched=await compare(password,user.password)
 
-        const userData = {
-          name: user.name,
-          email: user.email,
-          role: user.role,
-          id: user._id.toString(),
-        };
+        if(!isMatched){
 
-        return userData;
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/login",
-  },
-  callbacks: {
-    async session({ session, token }) {
-      if (token?.sub && token?.role) {
-        session.user.id = token.sub;
-        session.user.role = token.role;
+        throw new Error("password not matched")
       }
-      return session;
+
+      const userData={
+        name:user.name,
+        
+        email:user.email,
+        role:user.role,
+        id:user._id
+      }
+
+      return userData
+      }
+    })
+  ],
+
+  pages:{
+    signIn:'/login'
+  },
+  callbacks:{
+    async session({session,token}){
+     if(token?.sub && token?.role){
+      
+      session.user.id=token.sub;
+      session.user.role=token.role;
+       session.user.name =  session.user.name;
+     }
+     return session;
     },
-    async jwt({ token, user }) {
-      if (user) {
+
+    async  jwt({token,user}){
+      if(user){
         token.sub = user.id;
-        token.role = user.role;
+         token.role = user.role;
+          token.firstName = user.name?.split(' ')[0];
       }
       return token;
     },
-    async signIn({ user, account }) {
-      if (account?.provider === "google") {
+
+    signIn:async ({user,account})=>{
+      if(account?.provider=="google"){
         try {
-          const { email, name, image, id } = user as IUser;
-          await connectDB();
-          const existingUser = await User.findOne({ email });
-          if (!existingUser) {
-            await User.create({ email, name, image, authProviderId: id });
+          const {email,name,image,id}=user;
+          await connectDB()
+          const alreadyUser=await User.findOne({email});
+
+          if(!alreadyUser){
+            await User.create({email,name,image,authProviderId:id})
+          }else{
+            return true;
           }
-          return true;
-        } catch (error: any) {
-          throw new Error(error.message);
+
+        } catch (error) {
+          throw new Error("Error while creating user")
         }
       }
-      return true;
-    },
-  },
-};
 
-export { options as authOptions }; // Export the options for use in route.ts
+      if(account?.provider=="credentials"){
+        return true;
+      }else{
+        return false
+      }
+      
+
+    }
+  }
+})
