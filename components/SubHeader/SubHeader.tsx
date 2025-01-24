9e495e02a9fcb2ba5e3ecf392c1002c2faf5431d
AnusreeMandal <anusreemandal28@gmail.com>
2025-01-25 01:04:41 +0530

    icons added and downbar modified

--- components/SubHeader/SubHeader.tsx
+++ components/SubHeader/SubHeader.tsx
@@ -3,12 +3,16 @@ import React from 'react'
 import Link from 'next/link'
 import { useUser } from '@clerk/nextjs'
 import { useState, useEffect } from 'react'
+import { Home, Users, Calendar, Search } from 'lucide-react'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 import ITeam from '@/models/Team'
 import Modal from '../Modal/Modal'
 import { IUser } from '../expandableCards/card'
 import { IEvent } from '@/app/team/[id]/page'
 import Image from 'next/image'
 import SearchPage from '../search/Search'
+
+
 const SubHeader = () => {
   const { user, isLoaded } = useUser();
   const mongoId = user?.publicMetadata.mongoId as string
@@ -16,14 +20,9 @@ const SubHeader = () => {
   const [teams, setTeams] = useState<ITeam[]>([]);
   const [events, setEvents] = useState<IEvent[]>([]);
 
-  
-
-
-
   const [teamModal, setTeamModal] = useState(false);
   const [eventModal, setEventModal] = useState(false);
  
-
   const handleCloseEventModal = () => { setEventModal(false) }
   const handleOpenEventModal = () => { setEventModal(true) }
 
@@ -35,7 +34,6 @@ const SubHeader = () => {
   }
 
   useEffect(() => {
-
     const handleTeamsData = () => {
       const fetchData = async () => {
         if (isLoaded && user && mongoId) {
@@ -43,7 +41,6 @@ const SubHeader = () => {
             const result = await fetch(`/api/currentperson?id=${mongoId}&type=eandt`, {
               method: 'GET',
               headers: { 'Content-Type': 'application/json' },
-
             });
             const data = await result.json();
             setTeams(data.teams);
@@ -56,7 +53,6 @@ const SubHeader = () => {
             }
           } catch (error) {
             console.error('Error:', error);
-
           }
         }
       }
@@ -66,124 +62,125 @@ const SubHeader = () => {
     handleTeamsData();
   }, [isLoaded, user, mongoId]);
 
-
-  console.log("teams", teams)
-
   return (
-    <div className='absolute  top-0 left-[5vw]  right-[8vw] w-[50vw] bg-[#555a4a] h-[8vh] my-4 rounded-full items-center text-white flex flex-row justify-between'>
-      <Link href={'/'}>Home</Link>
-      <div className="mx-[5%]">
+    <div className='absolute top-0 left-[5vw] right-[8vw] w-[50vw] bg-[#555a4a] h-[8vh] my-4 rounded-full items-center text-white flex flex-row justify-between px-4'>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Link href={'/'}>
+              <Home className="w-6 h-6" />
+            </Link>
+          </TooltipTrigger>
+          
+          <TooltipContent side="bottom" align="center">
+            <p>Home</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
 
-        <button onClick={() => handleOpenTeamModal()}>Affliated Teams</button>
+      <div className="mx-[5%]">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <button onClick={() => handleOpenTeamModal()}>
+                <Users className="w-6 h-6" />
+              </button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Affiliated Teams</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
         <Modal isOpen={teamModal} onClose={handleCloseTeamModal}>
           {teams?.length > 0 && (teams?.map((team) => (
             team._id && <div className="p-4 flex flex-col md:flex-row justify-between items-center hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer" key={team._id.toString()}>
-
               <div className="flex gap-4 flex-col  md:flex-row ">
                 <div>
                   {team.timage ? <Image
-
                     width={100}
                     height={100}
                     src={team.timage}
                     alt={team.name}
-                  // className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
                   /> :
                     <img
-
                       width={100}
                       height={100}
                       src={'https://plus.unsplash.com/premium_vector-1683141200177-9575262876f7?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
                       alt={"team did'nt provide image"}
-                    //  className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
                     />}
                 </div>
                 <div className="">
-                  <h3
-
-                    className="font-medium text-neutral-800 dark:text-neutral-200 text-center md:text-left"
-                  >
+                  <h3 className="font-medium text-neutral-800 dark:text-neutral-200 text-center md:text-left">
                     {team.name}
                   </h3>
-                  <p
-
-                    className="text-neutral-600 dark:text-neutral-400 text-center md:text-left"
-                  >
+                  <p className="text-neutral-600 dark:text-neutral-400 text-center md:text-left">
                     {team.description}
                   </p>
                 </div>
               </div>
-
               <Link
                 onClick={() => setTeamModal(false)}
                 href={`/team/${team._id}?id=${team._id}`}
                 className="addButton px-4 py-2 text-sm rounded-full font-bold bg-gray-100 hover:bg-green-500 hover:text-white text-black mt-4 md:mt-0"
-
               >
                 View Team
               </Link>
-
             </div>
           )))}
-        </Modal></div>
-
+        </Modal>
+      </div>
       <div className="mx-[5%]">
-
-        <button onClick={() => handleOpenEventModal()}>Affliated Events</button>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <button onClick={() => handleOpenEventModal()}>
+                <Calendar className="w-6 h-6" />
+              </button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Affiliated Events</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
         <Modal isOpen={eventModal} onClose={handleCloseEventModal}>
           {events?.length > 0 && (events?.map((event) => (
             event._id && <div className="p-4 flex flex-col md:flex-row justify-between items-center hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer" key={event._id.toString()}>
-
               <div className="flex gap-4 flex-col  md:flex-row ">
                 <div>
                   {event.image ? <img
-
                     width={100}
                     height={100}
                     src={event.image}
                     alt={event.name}
-                  // className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
                   /> :
-                    <img
-
+                  <img
                       width={100}
                       height={100}
                       src={'https://plus.unsplash.com/premium_vector-1683141200177-9575262876f7?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
                       alt={"team did'nt provide image"}
-                    //  className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
                     />}
                 </div>
                 <div className="">
-                  <h3
-
-                    className="font-medium text-neutral-800 dark:text-neutral-200 text-center md:text-left"
-                  >
+                  <h3 className="font-medium text-neutral-800 dark:text-neutral-200 text-center md:text-left">
                     {event.name}
                   </h3>
-                  <p
-
-                    className="text-neutral-600 dark:text-neutral-400 text-center md:text-left"
-                  >
+                  <p className="text-neutral-600 dark:text-neutral-400 text-center md:text-left">
                     {event.description}
                   </p>
                 </div>
               </div>
-
               <Link
                 onClick={() => setEventModal(false)}
                 href={`/event/${event._id}?uid=${mongoId}`}
                 className="addButton px-4 py-2 text-sm rounded-full font-bold bg-gray-100 hover:bg-green-500 hover:text-white text-black mt-4 md:mt-0"
-
               >
                 View Event
               </Link>
-
             </div>
           )))}
-        </Modal></div>
+        </Modal>
+      </div>
 
-
-      {/*<Link href={'/academics'}>Assigned Works</Link> */}
       <SearchPage type={'all'} click={null} />
     </div>
   )
