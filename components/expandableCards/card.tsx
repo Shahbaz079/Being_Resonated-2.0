04b316265ca09948eb0c32392eac3a3384907083
Aitijhya Modak <aitijhyasps1@gmail.com>
2025-01-27 15:46:03 +0530

    Fixed issue while deploying

--- components/expandableCards/card.tsx
+++ components/expandableCards/card.tsx
@@ -102,7 +102,7 @@ export function ExpandableCardDemo({ users, cUser }: ExpandableCardDemoProps) {
 
         {users?.map((card) => (
 
-          <Dialog>
+          <Dialog key={`card-${card.name}-${id}`}>
             <DialogTrigger>
               <div
                 onClick={() => setActive(card)}
@@ -166,36 +166,4 @@ export function ExpandableCardDemo({ users, cUser }: ExpandableCardDemoProps) {
   );
 }
 
-// export const CloseIcon = () => {
-//   return (
-//     <motion.svg
-//       initial={{
-//         opacity: 0,
-//       }}
-//       animate={{
-//         opacity: 1,
-//       }}
-//       exit={{
-//         opacity: 0,
-//         transition: {
-//           duration: 0.05,
-//         },
-//       }}
-//       xmlns="http://www.w3.org/2000/svg"
-//       width="24"
-//       height="24"
-//       viewBox="0 0 24 24"
-//       fill="none"
-//       stroke="currentColor"
-//       strokeWidth="2"
-//       strokeLinecap="round"
-//       strokeLinejoin="round"
-//       className="h-4 w-4 text-black"
-//     >
-//       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-//       <path d="M18 6l-12 12" />
-//       <path d="M6 6l12 12" />
-//     </motion.svg>
-//   );
-// };
 
